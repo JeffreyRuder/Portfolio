081879b381c4750591a87d63aa523745f5bb7b81
Jeffrey Ruder <ruderjt@gmail.com>
2016-03-04 16:45:14 -0800

    Add style to project cards

--- app/helpers/clean-css.js
+++ app/helpers/clean-css.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function cleanCSS(params) {
-  return new Ember.Handlebars.SafeString("background: url(assets/images/" + params + ") no-repeat left top;background-size:fill;");
+  return new Ember.Handlebars.SafeString("background: url(assets/images/" + params + ") no-repeat top left;background-size:fill;");
 }
 
 export default Ember.Helper.helper(cleanCSS);
