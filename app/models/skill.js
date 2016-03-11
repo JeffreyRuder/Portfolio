ce526011fad0c58409892893e08c50271ad992b9
Jeffrey Ruder <ruderjt@gmail.com>
2016-03-10 19:26:13 -0800

    Improve styling and sort skills into categories

--- app/models/skill.js
+++ app/models/skill.js
@@ -3,5 +3,10 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   image: DS.attr(),
+  isLanguage: DS.attr(),
+  isTesting: DS.attr(),
+  isFramework: DS.attr(),
+  isBuildTool: DS.attr(),
+  isDatabase: DS.attr(),
   projects: DS.hasMany('project')
 });
